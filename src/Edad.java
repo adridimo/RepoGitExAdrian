7bf393bdabeed3083c095d80a81b4e2a7288a670
Adrian <adridimo04@gmail.com>
2023-05-24 14:43:19 +0200

    adada

---
+++ src/Edad.java
@@ -0,0 +1,22 @@
+
+public class Edad {
+	int menores=0;
+	int mayores=0;
+	int jubilados=0;
+	int []numeros;
+	public Edad(int[] numeros) {
+		super();
+		this.numeros = numeros;
+	}
+	public String getedad(int []numeros) {
+		
+		for (int i =0; i<numeros.length;i++) {
+			if (numeros[i] <18) {
+				
+			}
+			
+		}
+		String frase = null; 
+		return frase;
+	}
+}
---
+++ src/PrincipalApp.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class PrincipalApp {
+
+	public static void main(String[] args) {
+		int [] numeros  = {31,3,64,14,40,78,21,11} ;
+		Edad resultado = new Edad(numeros);
+		
+	}
+
+}
